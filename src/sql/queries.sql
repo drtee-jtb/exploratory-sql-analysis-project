3886026c037dd51dd226cf058998d6f44f17305a
drtee-jtb <mine4dot@gmail.com>
2026-03-17 18:13:31 +0000

    SQL Project

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -1,27 +1,11 @@
--- PLEASE READ THIS BEFORE RUNNING THE EXERCISE
-
--- ⚠️ IMPORTANT: This SQL file may crash due to two common issues: comments and missing semicolons.
-
--- ✅ Suggestions:
--- 1) Always end each SQL query with a semicolon `;`
--- 2) Ensure comments are well-formed:
---    - Use `--` for single-line comments only
---    - Avoid inline comments after queries
---    - Do not use `/* */` multi-line comments, as they may break execution
-
--- -----------------------------------------------
--- queries.sql
--- Complete each mission by writing your SQL query
--- directly below the corresponding instruction
--- -----------------------------------------------
 
 SELECT * FROM regions;
 SELECT * FROM species;
 SELECT * FROM climate;
 SELECT * FROM observations;
 
-
 -- MISSION 1
+-- Your query here;
 SELECT * FROM observations limit 10;
 
 -- MISSION 2
